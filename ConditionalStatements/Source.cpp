8efc767bb14c70ec2a6b4dad0817cfb1dcfd1aaf
Thomas Wiltshire <thomaswiltshire@Kochanski.local>
2017-03-01 15:35:54 +1100

    First Commit.

---
+++ ConditionalStatements/Source.cpp
@@ -0,0 +1,196 @@
+// Question 1 //
+//#include <iostream>
+//
+//void main()
+//{
+//	int x = 0;
+//	int y = 0;
+//
+//	if (y == 0)
+//	{
+//		x = 100;
+//	}
+//
+// system("pause");
+//}
+// Question 1 //
+
+// Question 2 //
+//#include <iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	int x;
+//	int y;
+//
+//	cin >> x >> y;
+//
+//	if (y > x)
+//	{
+//		cout << y;
+//	}
+//	else if (x > y)
+//	{
+//		cout << x;
+//	}
+//
+//	system("pause");
+//}
+// Question 2 //
+
+// Question 3 // //Not Complete// 
+//#include <iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	int a;
+//	int b;
+//	int c;
+//	int d;
+//	int e;
+//
+//	cin >> a >> b >> c >> d >> e;
+//	
+//	int result = result + a + b + c + d + e;
+//
+//	if (result > 0)
+//	{
+//		
+//	}
+//	else if (result < 0)
+//	{
+//
+//	}
+//	else
+//	{
+//		cout << a << b << c << d << e;
+//	}
+//
+//	system("pause");
+//}
+// Question 3 // //Not Complete//
+
+// Question 4 //
+//#include <iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	int choice = 1;
+//
+//	switch(choice)
+//	{
+//		case 1:
+//			cout << "1";
+//		break;
+//
+//		case 2:
+//		case 3:
+//			cout << "2 or 3";
+//		break;
+//
+//		case 4:
+//			cout << "4";
+//		break;
+//
+//		default:
+//			cout << "Invalid";
+//		break;
+//	}
+//
+//	system("pause");
+//}
+// Question 4 // 
+
+// Question 5 //
+//#include <iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	int x;
+//	int y;
+//
+//	int result = (x == 0) ? y = 0 : y = 10 / x;
+//
+//	system("pause");
+//}
+// Question 5 // 
+
+// Question 6 //
+//#include <iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	int x;
+//	int y;
+//	char z;
+//	int result;
+//
+//	cout << "Please type in 2 numbers and then a matematical operation character";
+//	cin >> x >> y;
+//
+//	if (z == '+')
+//	{
+//		result = x + y;
+//	}
+//	else if (z == '-')
+//	{
+//		result = x - y;
+//	}
+//	else if (z == '*')
+//	{
+//		result = x * y;
+//	}
+//	else if (z == '/')
+//	{
+//		result = x / y;
+//	}
+//	else if (z == '%')
+//	{
+//		result = x % y;
+//	}
+//
+//	cout << "Equal: " << result;
+//
+//	system("pause");
+//}
+// Question 6 // 
+
+// Question 7 //
+//#include <iostream>
+//using namespace std;
+//
+//void main()
+//{
+//	int month;
+//	int result;
+//
+//	cin >> month;
+//
+//	if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+//	{
+//		result = 31;
+//		cout << "This month has: " << result << " days" << endl;
+//	}
+//	else if (month == 4 || month == 6 || month == 9 || month == 11)
+//	{
+//		result = 30;
+//		cout << "This month has: " << result << " days" << endl;
+//	}
+//	else if (month == 2)
+//	{
+//		result = 28;
+//		cout << "This month has: " << result << " days" << endl;
+//	}
+//	else
+//	{
+//		cout << "Not a valid month value" << endl;
+//	}
+//
+//	system("pause");
+//}
+// Question 7 //
